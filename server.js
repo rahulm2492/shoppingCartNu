4892fcef5353eafbea8779f2c19ed3ef11af44bb
Rahul Maheshwari <rahulm@qburst.com>
2017-12-27 02:41:42 +0530

    Server Config Updated

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname,  'dist')));
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(8080, '127.0.0.1',function(){
+app.listen(server_port, server_host,function(){
 console.log('Listening on port %d', server_port);
 });
 
